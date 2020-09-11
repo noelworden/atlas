dbda549796626ac09f50f655cae1a0f92705271e
Noel Worden <noelworden@gmail.com>
2020-09-10 19:40:33 -0600

    [FEAT] Added Docker
    All setup necessary to dockerize the project. The following steps will
    start the project in docker, assuming Docker is install on the dev
    machine:
    - docker-compose build
    - docker-compose up

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :atlas, Atlas.Repo,
   username: "postgres",
   password: "postgres",
   database: "atlas_dev",
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
