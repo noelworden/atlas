b181adc975cd08a1847a80d662bb34abf9eced27
Noel Worden <noelworden@gmail.com>
2020-09-16 10:49:25 -0600

    [FEAT] CRUD actions for `destination` module
    The second set of files generated by `phx.gen.html`. Includes
    controller, views, and tests. No additional styling has been done to
    the view files, that will come at a later time.

---
+++ lib/atlas_web/controllers/destination_controller.ex
@@ -0,0 +1,62 @@
+defmodule AtlasWeb.DestinationController do
+  use AtlasWeb, :controller
+
+  alias Atlas.Mapping
+  alias Atlas.Mapping.Destination
+
+  def index(conn, _params) do
+    destinations = Mapping.list_destinations()
+    render(conn, "index.html", destinations: destinations)
+  end
+
+  def new(conn, _params) do
+    changeset = Mapping.change_destination(%Destination{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"destination" => destination_params}) do
+    case Mapping.create_destination(destination_params) do
+      {:ok, destination} ->
+        conn
+        |> put_flash(:info, "Destination created successfully.")
+        |> redirect(to: Routes.destination_path(conn, :show, destination))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    destination = Mapping.get_destination!(id)
+    render(conn, "show.html", destination: destination)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    destination = Mapping.get_destination!(id)
+    changeset = Mapping.change_destination(destination)
+    render(conn, "edit.html", destination: destination, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "destination" => destination_params}) do
+    destination = Mapping.get_destination!(id)
+
+    case Mapping.update_destination(destination, destination_params) do
+      {:ok, destination} ->
+        conn
+        |> put_flash(:info, "Destination updated successfully.")
+        |> redirect(to: Routes.destination_path(conn, :show, destination))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", destination: destination, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    destination = Mapping.get_destination!(id)
+    {:ok, _destination} = Mapping.delete_destination(destination)
+
+    conn
+    |> put_flash(:info, "Destination deleted successfully.")
+    |> redirect(to: Routes.destination_path(conn, :index))
+  end
+end
--- lib/atlas_web/router.ex
+++ lib/atlas_web/router.ex
@@ -17,6 +17,7 @@ defmodule AtlasWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/destinations", DestinationController
   end
 
   # Other scopes may use custom stacks.
