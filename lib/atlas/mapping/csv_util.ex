1e41d51f5e2b89e955343f20cbb544e78dba4e0f
Noel Worden <noelworden@gmail.com>
2020-09-22 07:44:10 -0600

    [DATA] Create seed data for `destinations` table
    Added the NimbleCSV library to parse CSVs. Most of the heavy lifting is
    done by the CSVUtil module, and called in `seeds.exs`.
    
    Seeds can be run by this command:
    
    `docker-compose exec web mix run priv/repo/seeds.exs`

---
+++ lib/atlas/mapping/csv_util.ex
@@ -0,0 +1,69 @@
+defmodule Atlas.Mapping.CSVUtil do
+  @moduledoc """
+    Utility module to ingest `destination.csv`
+  """
+
+  alias NimbleCSV.RFC4180, as: CSV
+  alias Atlas.{Mapping.Destination, Repo}
+
+  def column_data(file) do
+    column_names = get_column_names(file)
+
+    file
+    |> File.stream!()
+    |> CSV.parse_stream(skip_headers: true)
+    |> Enum.map(fn row ->
+      row
+      |> Enum.with_index()
+      |> Map.new(fn {val, num} -> {column_names[num], val} end)
+      |> create_or_skip()
+    end)
+  end
+
+  def get_column_names(file) do
+    file
+    |> File.stream!()
+    |> CSV.parse_stream(skip_headers: false)
+    |> Enum.fetch!(0)
+    |> Enum.with_index()
+    |> Map.new(fn {val, num} -> {num, val} end)
+  end
+
+  def create_or_skip(row) do
+    case Repo.get_by(Destination,
+           latitude: row["latitude"],
+           longitude: row["longitude"]
+         ) do
+      nil ->
+        Repo.insert(
+          %Destination{}
+          |> Destination.changeset(%{
+            longitude: Decimal.new(row["longitude"]),
+            latitude: Decimal.new(row["latitude"]),
+            name: row["name"],
+            description: row["description"],
+            spinner_friendly: row["spinner_friendly"],
+            lake: row["lake"],
+            less_than_one_hour: row["less_than_one_hour"],
+            one_to_three_hours: row["one_to_three_hours"],
+            greater_than_three_hours: row["greater_than_three_hours"],
+            car_friendly: row["car_friendly"],
+            hike_in: row["hike_in"],
+            allows_dogs: row["allows_dogs"],
+            dogs_off_leash: row["dogs_off_leash"],
+            season_spring: row["season_spring"],
+            season_summer: row["season_summer"],
+            season_fall: row["season_fall"],
+            season_winter: row["season_winter"],
+            car_camp: row["car_camp"],
+            backpack_camp: row["backpack_camp"],
+            fee: row["fee"],
+            ice_fishing: row["ice_fishing"]
+          })
+        )
+
+      destination ->
+        {:ok, destination}
+    end
+  end
+end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,6 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Seed `destinations` table from CSV
+Atlas.Mapping.CSVUtil.column_data("priv/repo/data/destinations.csv")
