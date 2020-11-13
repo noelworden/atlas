f1b39316e1bd1462dbb1daabf6c5e510474f596b
Noel Worden <noelworden@gmail.com>
2020-11-13 15:57:47 -0700

    [TOOL] Add IEx sample preference file
    This adds a sample file, that a dev can then copy into an `.iex.exs`
    file. This file has preemtively added to `.gitignore` and also included
    as a docker volume.

---
+++ .iex.sample.exs
@@ -0,0 +1,7 @@
+# Example imports/aliases that can be used in an `.iex.exs` file, which is read
+# on startup of an IEx shell. Any other commonly used imports/aliases/variables
+# can also be added.
+
+# import Ecto.Query
+# alias Atlas.Repo
+# alias Atlas.Mapping.Destination
